9159dd61b08e6996117b1bb51f7a97904a9f4715
Nicolas Kamien <nicolas@kamien.com.br>
2022-06-24 13:57:29 -0300

    Mongoose Part 3

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
@@ -15,15 +14,31 @@ connect.then((db) => {
     .then((dish) => {
       console.log(dish);
 
-      Dishes.find({}).exec();
+      return Dishes.findByIdAndUpdate(dish._id, {
+        $set: { description: 'Updated test' }
+      }, {
+        new: true
+      })
+        .exec();
     })
-    .then((dishes) => {
-      console.log(dishes);
+    .then((dish) => {
+      console.log(dish);
+
+      dish.comments.push({
+        rating: 5,
+        comment: 'I\'m getting a sinking feeling!',
+        author: 'Leonardo di Carpaccio'
+      });
+
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
 
       return Dishes.remove({});
     })
     .then(() => {
-      return mongoose.connections.close();
+      return mongoose.connection.close();
     })
     .catch((err) => {
       console.log(err);
